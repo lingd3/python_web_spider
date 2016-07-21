07baf6ec6b25e96bb4e6582441676e79b3db1937
linqiaonihao <824928207@qq.com>
2016-07-21 21:48:59 +0800

    spider

---
+++ baike_spider/html_downloader.py
@@ -0,0 +1,23 @@
+# coding:UTF8
+import urllib2
+
+# url下载器
+
+class HtmlDownloader(object):
+    
+    
+    def download(self, url):
+        if url is None:
+            return None
+        
+        response = urllib2.urlopen(url)
+        
+        if response.code != 200:
+            return None
+        
+        return response.read()
+    
+    
+
+
+
